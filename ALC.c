dffff63e6ef7bce0131c551e6231e945629a3d7f
eights <oliviaawongg@gmail.com>
2018-12-30 23:05:40 -0800

    Included all accel axes for readings, cleaned up printing, added threshold to stop

--- ALC.c
+++ ALC.c
@@ -1,5 +1,4 @@
- /**
- * @file ALC.c
+/* @title ALC.c
  *
  * @brief      Actuated landing control (ALC)--Use a servo to rotate the rover until it's upright.
  *
@@ -7,24 +6,27 @@
  *
  * @date       12/29/2018
  * 1. make loop to PRINT DOWN ACCEL
- * 2. RESET SERVO to 0
+ * 2. keep 
  * 3.
  */
 
 #include <stdio.h>
 #include <signal.h>
+#include <math.h>
 #include <getopt.h>
 #include <rc/mpu.h>
 #include <rc/time.h>
 #include <unistd.h>
 
-#define I2C_BUS 2 // Bus for Beaglebone blue
+#define G 9.8 // Gravity m/s^2
+#define ACCEL_READS 5 // # of times to read accelerometer before averaging
+#define ACCEL_READ_INTERVAL 500000 // Microseconds
+#define X 0
+#define Y 1
+#define Z 2
 
 static int running = 0;
-static unsigned int accel_read_interval = 500000; // Microseconds
-static int accel_reads = 5; // # of times to read accelerometer before averaging
-
-rc_mpu_data_t mpu_data; // Struct to hold new MPU data
+static rc_mpu_data_t mpu_data; // Struct to hold new MPU data
 
 // Interrupt handler to catch ctrl-c
 static void __signal_handler(__attribute__ ((unused)) int dummy) {
@@ -32,29 +34,38 @@ static void __signal_handler(__attribute__ ((unused)) int dummy) {
 	return;
 }
 
-double read_accel() {
+void *read_accel(double *buf) {
 	/*
-	* It's noisy and fluctuates a lot, so we average out the last 'accel_reads' readings
+	* Reads accelerometer Z data, averages last 'ACCEL_READS' readings to reduce fluctuation
 	*/
+	double sums[3] = {0};
 
-	double readings[accel_reads]; 
-	double average = 0;
+	printf("Accelerometer readings: \n");
+	printf("    X     |     Y     |     Z    |\n");
 
-	printf("Accelerometer Z readings: ");
-
-	for (int i=0; i<accel_reads; i++) {
+	for (int i=0; i<ACCEL_READS; i++) {
 		if(rc_mpu_read_accel(&mpu_data)<0) {
-			printf("read accel data failed\n");
+			printf("Read accel data failed\n");
 		}
+		for (int j=0; j<3; j++) {
+			// Print X, Y, Z accel data
+			printf("%f | ", mpu_data.accel[j]);
+			sums[j] += mpu_data.accel[j]; // Take sums to avg later
+		}
+		printf("\n");
 		// We only care about the Z acceleration which is accel[2]
-		printf("%f, ", mpu_data.accel[2]);
-		average += mpu_data.accel[2];
-		usleep(accel_read_interval); // Wait to take next reading
+		
+		usleep(ACCEL_READ_INTERVAL); // Wait to take next reading
 	}
 
-	average /= accel_reads; // Take the average
-	printf("\nAverage: %f\n", average);
-	return average;
+	printf("\nAverages:\n");
+	printf("    X     |     Y     |     Z    |\n");
+
+	for (int i=0; i<3; i++) {
+		buf[i] = sums[i] / ACCEL_READS; // Take the average and assign it to buf
+		printf("%f | ", buf[i]);
+	}
+	printf("\n");
 
 }
 int main(int argc, char *argv[]) {
@@ -65,16 +76,17 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	read_accel();
+	double accel_data[3] = {0};
+
+	read_accel(accel_data);
+
 	// Set signal handler so the loop can exit cleanly
 	signal(SIGINT, __signal_handler);
 	running = 1;
-/*
-	while (running) {
-		printf("%6.2f %6.2f %6.2f |",
-		mpu_data.accel[0],\
-		mpu_data.accel[1],\
-		mpu_data.accel[2]);
 
-		}*/
+	while (running && fabs(G - accel_data[Z]) > 0.1) { 
+		// Z !~ 9.8
+		read_accel(accel_data);
+	}
+	printf("IM RIGHT SIDE UP");
 }
