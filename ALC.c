7a1e7afcfdcbfd058c5c350695d9a600687a09f5
eights <oliviaawongg@gmail.com>
2019-01-01 16:33:02 -0800

    change motor direction based on accel data

--- ALC.c
+++ ALC.c
@@ -6,7 +6,7 @@
  *
  * @date       12/29/2018
  * 1. make loop to PRINT DOWN ACCEL
- * 2. keep 
+ * 2. keep
  * 3.
  */
 
@@ -14,16 +14,24 @@
 #include <signal.h>
 #include <math.h>
 #include <getopt.h>
+#include <rc/adc.h>
 #include <rc/mpu.h>
 #include <rc/time.h>
+#include <rc/servo.h>
 #include <unistd.h>
 
 #define G 9.8 // Gravity m/s^2
-#define ACCEL_READS 5 // # of times to read accelerometer before averaging
+#define ACCEL_READS 3 // # of times to read accelerometer before averaging
 #define ACCEL_READ_INTERVAL 500000 // Microseconds
 #define X 0
 #define Y 1
 #define Z 2
+#define SERVO_POS 1.0
+#define NUM_PULSES 5
+#define PULSE_FREQ_HZ 50
+#define PULSE_WIDTH_US 12 // Microseconds. idk if this changes anything
+#define SERVO_CH 8
+
 
 static int running = 0;
 static rc_mpu_data_t mpu_data; // Struct to hold new MPU data
@@ -32,6 +40,42 @@ static rc_mpu_data_t mpu_data; // Struct to hold new MPU data
 static void __signal_handler(__attribute__ ((unused)) int dummy) {
 	running = 0;
 	return;
+}
+int run_motor(double direction) {
+	// Init servo
+	// Read adc to make sure battery is connected
+	if(rc_adc_init()) {
+		fprintf(stderr, "ERROR: failed to run rc_adc_init()\n");
+		return -1;
+	}
+	if(rc_adc_batt() < 6.0) {
+		fprintf(stderr,"ERROR: battery disconnected or insufficiently charged to drive servos\n");
+		return -1;
+	}
+	rc_adc_cleanup();
+
+	// Initialize PRU
+	if(rc_servo_init()) return -1;
+
+	// Turn on power
+	printf("Turning On 6V Servo Power Rail\n");
+	rc_servo_power_rail_en(1);
+
+	// Send pulse(s)
+	printf("Moving servo to %f...\n", SERVO_POS * direction);
+
+	for (int i=0; i<NUM_PULSES; i++) {
+		if(rc_servo_send_pulse_normalized(SERVO_CH, SERVO_POS * direction) == -1) return -1;
+		rc_usleep(1000000 / PULSE_FREQ_HZ);
+	}
+
+	printf("Turn off/clean up servo\n");
+
+	// Turn off power rail and cleanup
+	rc_servo_power_rail_en(0);
+	rc_servo_cleanup();
+	printf("Finished servo move\n");
+	return 1;
 }
 
 void *read_accel(double *buf) {
@@ -53,8 +97,7 @@ void *read_accel(double *buf) {
 			sums[j] += mpu_data.accel[j]; // Take sums to avg later
 		}
 		printf("\n");
-		// We only care about the Z acceleration which is accel[2]
-		
+
 		usleep(ACCEL_READ_INTERVAL); // Wait to take next reading
 	}
 
@@ -65,10 +108,11 @@ void *read_accel(double *buf) {
 		buf[i] = sums[i] / ACCEL_READS; // Take the average and assign it to buf
 		printf("%f | ", buf[i]);
 	}
-	printf("\n");
+	printf("\n\n");
 }
 
 int main(int argc, char *argv[]) {
+	// Init sensor reading
 	rc_mpu_config_t mpu_conf = rc_mpu_default_config();
 
 	if(rc_mpu_initialize(&mpu_data, mpu_conf)) {
@@ -82,23 +126,26 @@ int main(int argc, char *argv[]) {
 	signal(SIGINT, __signal_handler);
 	running = 1;
 
-	while (running) { 
-		printf("Move me\n");
-		sleep(2); // Allow servo time to move
+	while (running) {
 
-		// Read accelerometer 
+		// Read accelerometer
 		printf("Starting reading...\n");
 		read_accel(accel_data);
 
 		// Determine which way to move based on accel reading
-		if (fabs(G - accel_data[Z]) > 0.1) {
+		if (fabs(G - accel_data[Z]) < 0.13) {
 			// Z ~ 9.8
+			// TODO: test 0.13 value, make sure it works for servo sides
+			printf("IM RIGHT SIDE UP\n");
 			break;
 		} else if(accel_data[X] > 0) {
 			printf("TURN ME COUNTER CLOCKWISE\n");
+			run_motor(1.0);
+
 		} else {
 			printf("TURN ME CLOCKWISE\n");
+			run_motor(-1.0);
 		}
+		rc_usleep(500000); // Sleep half a second to make sure motor is done moving
 	}
-	printf("IM RIGHT SIDE UP\n");
 }
